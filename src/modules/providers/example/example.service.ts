b8d9fe402c4d01331acb0a567cbfaa8303374bf1
José Contreras <jocontrera16@gmail.com>
2023-07-12 00:20:10 -0400

    added: observable resolver ready

--- src/modules/providers/example/example.service.ts
+++ src/modules/providers/example/example.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService as Axios } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
   constructor(private readonly axios: Axios) {}
 
-  async getHealthCheck(): Promise<{ ping: string; data: any }> {
-    const request = await this.axios.axiosRef.get(
+  async getHealthCheck(): Promise<{
+    ping: string;
+    data: any;
+    observable: any;
+  }> {
+    const awaitedRequest = await this.axios.axiosRef.get(
       'https://animechan.xyz/api/random/anime?title=naruto',
     );
 
-    const { data } = request;
+    const observableRequest = await firstValueFrom(
+      this.axios.get('https://animechan.xyz/api/random/anime?title=naruto'),
+    );
+
+    const { data: observable } = observableRequest;
+
+    const { data } = awaitedRequest;
 
     return {
       ping: 'pong',
       data,
+      observable,
     };
   }
 }
